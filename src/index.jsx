48a0f881859ea0e87ba504dfcd1672410a40f547
sulav kharel <kharel.sulav2016@gmail.com>
2019-07-18 23:39:28 +0900

    typo

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,7 @@ const identityReducer = (state = null) => state;
 
 const initialState = {
   messages: [],
-  channels: ['general', 'react', 'tokyo'],
+  channels: ['general', 'ML', 'tokyo'],
   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
   selectedChannel: 'general'
 };
